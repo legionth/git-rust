b860b10eb2a566b4386d4cbaac66e11de2e241a2
Niels Theen <niels.theen@gmail.com>
2022-10-16 18:03:08 +0200

    First version of commit

--- src/lib.rs
+++ src/lib.rs
@@ -1,9 +1,28 @@
 
 pub struct Repository {
     name: String,
+    history: Vec<Commit>
 }
 
 pub struct Commit {
     id: String,
     message: String
+}
+
+impl Commit {
+    pub fn new(message: String) -> Commit
+    {
+        let id = String::from("1d");
+
+        Commit {
+            id,
+            message
+        }
+    }
+}
+
+pub fn commit(mut repository: Repository, message: String) {
+    let commit: Commit = Commit::new(message);
+
+    repository.history.push(commit);
 }
